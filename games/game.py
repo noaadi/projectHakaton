5d818767e285512649b102db15f071bbae2dc023
IamShaharFar <shaharfar16@gmail.com>
2025-03-24 16:40:22 +0200

    added return to menu button

--- games/game.py
+++ games/game.py
@@ -7,18 +7,16 @@ class Game(ABC):
     def __init__(self, screen):
         self.screen = screen
         self.running = True
+        self.return_to_menu = False  
 
     @abstractmethod
     def handle_event(self, event):
-        """Handle user input (must be implemented in child classes)."""
         pass
 
     @abstractmethod
     def update(self):
-        """Update game logic (must be implemented in child classes)."""
         pass
 
     @abstractmethod
     def render(self):
-        """Render game visuals (must be implemented in child classes)."""
         pass
